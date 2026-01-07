41f0ee055aa807830c4a97350765f422fec1542e
Mayr, Hannes <hmayr@ikp.tu-darmstadt.de>
2026-01-07 11:14:52 +0100

    Include uncertainties of norm factors.

--- napytau/core/tau_simple.py
+++ napytau/core/tau_simple.py
@@ -6,21 +6,40 @@ from napytau.core.time import calculate_times_from_distances_and_relative_veloci
 
 def calc_tau_simple(dataset: DataSet) -> np.ndarray:
     normalisation_factors = dataset.get_datapoints().get_calibrations().get_values()
+    normalisation_factors_uncertainties = (
+        dataset.get_datapoints().get_calibrations().get_errors()
+    )
     intensities_unshifted = (
         dataset.get_datapoints().get_unshifted_intensities().get_values()
         * normalisation_factors
     )
-    intensities_unshifted_uncertainties = (
-        dataset.get_datapoints().get_unshifted_intensities().get_errors()
-        * normalisation_factors
+    intensities_unshifted_uncertainties = np.sqrt(
+        (
+            dataset.get_datapoints().get_unshifted_intensities().get_errors()
+            * normalisation_factors
+        )
+        ** 2
+        + (
+            normalisation_factors_uncertainties
+            * dataset.get_datapoints().get_unshifted_intensities().get_values()
+        )
+        ** 2
     )
     intensities_shifted = (
         dataset.get_datapoints().get_shifted_intensities().get_values()
         * normalisation_factors
     )
-    intensities_shifted_uncertainties = (
-        dataset.get_datapoints().get_shifted_intensities().get_errors()
-        * normalisation_factors
+    intensities_shifted_uncertainties = np.sqrt(
+        (
+            dataset.get_datapoints().get_shifted_intensities().get_errors()
+            * normalisation_factors
+        )
+        ** 2
+        + (
+            dataset.get_datapoints().get_shifted_intensities().get_values()
+            * normalisation_factors_uncertainties
+        )
+        ** 2
     )
     distances = dataset.get_datapoints().get_distances().get_values()
     flight_times = calculate_times_from_distances_and_relative_velocity(dataset)
@@ -44,20 +63,28 @@ def calc_tau_simple(dataset: DataSet) -> np.ndarray:
         differential_shifted_mean[i] = (
             intensities_shifted[i + 1] - intensities_shifted[i]
         ) / (flight_times[i + 1] - flight_times[i])
+
         differential_shifted_mean_uncertainties[i] = np.sqrt(
-            (1 / (flight_times[i + 1] - flight_times[i]) ** 2)
-            * (intensities_shifted[i + 1] ** 2 + intensities_shifted[i] ** 2)
+            (1 / (flight_times[i + 1] - flight_times[i])) ** 2
+            * (
+                intensities_shifted_uncertainties[i + 1] ** 2
+                + intensities_shifted_uncertainties[i] ** 2
+            )
         )
 
     tau_simple_values = unshifted_mean / differential_shifted_mean
     tau_simple_uncertainties = np.sqrt(
         (unshifted_mean_uncertainties / differential_shifted_mean) ** 2
-        + (differential_shifted_mean * unshifted_mean / differential_shifted_mean**2)
+        + (
+            differential_shifted_mean_uncertainties
+            * unshifted_mean
+            / differential_shifted_mean**2
+        )
         ** 2
     )
-    tau_simple = np.average(tau_simple_values, weights=1 / tau_simple_uncertainties**2)
-    tau_simple_uncertainty = np.sqrt(np.sum(tau_simple_uncertainties**2)) / len(
-        tau_simple_uncertainties
+    tau_simple, sum_of_weights = np.average(
+        tau_simple_values, weights=1 / tau_simple_uncertainties**2, returned=True
     )
+    tau_simple_uncertainty = 1 / np.sqrt(sum_of_weights)
 
     return tau_simple, tau_simple_uncertainty
